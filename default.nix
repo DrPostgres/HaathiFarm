cb79d50de0eadcb5dfc9db56f9f0160c10398f3c
Gurjeet Singh <gurjeet@singh.im>
2022-08-26 01:27:35 -0700

    Use Darwin binutils provided by Nixpkgs
    We used to use /usr/bin/ar, an impure path, because the `ar` command in
    the binutils package used to cause errors. This patch fixes that by
    providing not just `ar`, but other commands as well from the
    Nixpkgs-supported binutils specifically maintained for Darwin.
    
    This patch includes many other changes as well, mostly cosmetic. But
    these are mostly in preparation for the patch we need, to support
    `make check`; being discussed in the Postgres community:
    
    https://www.postgresql.org/message-id/CABwTF4VBTKLORbKMOT8qi7W76vQ5hsV73Q%2BraPpXFveoiWUwBQ%40mail.gmail.com

--- default.nix
+++ default.nix
@@ -1,3 +1,8 @@
+/*
+ * The purpose of the code in this file is to provide a deterministic build
+ * environment for building Postgres, using different versions of the packages
+ * needed to build Postgres.
+ */
 let
   config = rec {
     # The root where all farm animals' files will be stored.
@@ -39,20 +44,25 @@ let
 
   stdenv_pkg = nixpkgs_derivations.stdenv;
 
+  # Is the platform target a Darwin derivative
+  is_darwin = stdenv_pkg.hostPlatform.isDarwin;
+
   # Binaries
   bash_path      = toString nixpkgs_derivations.bash;
-  binutils_path  = toString nixpkgs_derivations.binutils;
+  binutils_path  = toString (if is_darwin then
+                              nixpkgs_derivations.darwin.cctools
+                            else
+                              nixpkgs_derivations.binutils);
   bison_path     = toString nixpkgs_derivations.bison;
   coreutils_path = toString nixpkgs_derivations.coreutils;
+  diffutils_path = toString nixpkgs_derivations.diffutils;
   flex_path      = toString nixpkgs_derivations.flex;
   gawk_path      = toString nixpkgs_derivations.gawk;
+  gcc_path       = toString nixpkgs_derivations.gcc;
   gnugrep_path   = toString nixpkgs_derivations.gnugrep;
   gnumake_path   = toString nixpkgs_derivations.gnumake;
   gnused_path    = toString nixpkgs_derivations.gnused;
-  gcc_path       = toString nixpkgs_derivations.gcc;
   perl_path      = toString nixpkgs_derivations.perl;
-
-
 
   # Libraries
   readline_out_path = toString nixpkgs_derivations.readline.out;
@@ -60,9 +70,9 @@ let
   zlib_out_path     = toString nixpkgs_derivations.zlib.out;
   zlib_dev_path     = toString nixpkgs_derivations.zlib.dev;
 
-  # Dev dependencies; for when using nix-shell
-  which_path     = toString nixpkgs_derivations.which;
-  less_path     = toString nixpkgs_derivations.less;
+  # Dev dependencies; for when using interactively, say, in nix-shell
+  which_path = toString nixpkgs_derivations.which;
+  less_path  = toString nixpkgs_derivations.less;
 
   pgbuildfarm_src = fetchGit {
     url = "git@github.com:PGBuildFarm/client-code.git";
@@ -75,10 +85,26 @@ let
   source_nix = readFile ./default.nix; 
   source_nix_in_store = toFile "default.nix" source_nix;
 
+  # The driver script
   builder = toFile "builder.sh" ''
     #!/usr/bin/env bash
 
-    set -eu
+    # Exit shell on error
+    set -e
+
+    # Read the optional environment variables, before we prohibit access of
+    # undefined variables.
+    #
+    # If we intend to inherit any of the environment variables, this is the
+    # place to inherit them with a new name. Using a name different than the
+    # original is desirable, since we want to catch any of the packages that
+    # may depend on the values of such variables.
+    #
+    # For example:
+    # in_nix_shell=$IN_NIX_SHELL
+
+    # Throw error on undefined variables
+    set -u
 
     _log_file="$out/builder.log"
     function log_file() { echo "$_log_file";}
@@ -95,9 +121,11 @@ let
 
     #### Emit enough info for later troubleshooting ####
 
-    # Change PATH enough to use `mkdir`. This is necessary to
-    # let us capture logging info in a file inside $out.
-    export PATH="$coreutils_path/bin/:$PATH"
+    # Change PATH enough to use `mkdir`, and a few other commands, necessary for
+    # logging.
+    #
+    # Note that we _don't_ use the $PATH passed to us in the environment.
+    export PATH="$coreutils_path/bin"
     mkdir "$out"
 
     info "uname -a: $(uname -a)"
@@ -106,8 +134,11 @@ let
     info Nix language version is ${toString langVersion}.
     info Source Nix file is at ${source_nix_in_store}.
     info "OS {user; groups}: {$(whoami); $(groups)}"
-    info Exported and unexported shell variables:
-    declare -p >> "$(log_file)"
+    info "Exported and unexported shell variables: $(printf '\n'; declare -p)"
+
+    # Record the checksum of impure files that may be used by Postgres
+    info Checksums of some of the files from outside Nix Store:
+    info $(sha256sum /bin/sh) # Used by many scripts, and pg_ctl, in particular.
 
     rc=0
     touch "$FARM_ROOT/touched_from_Nix_builder.txt" || rc=$?
@@ -118,62 +149,71 @@ let
 
     #### Prepare to build Postgres ####
 
-    f="$binutils_path/nix-support/orig-bintools"
-    binutils_orig=
-    if [[ -f "$f" ]]; then
-      binutils_orig=$(cat "$f")
-    fi
-    info "binutils_orig: $binutils_orig"
+    # Place binaries in $PATH.
+    #
+    # Note that we _don't_ use the $PATH passed to us in the environment.
+    export PATH="$bash_path/bin:$binutils_path/bin:$bison_path/bin:$coreutils_path/bin:$diffutils_path/bin:$flex_path/bin/:$gawk_path/bin:$gcc_path/bin:$gnugrep_path/bin:$gnumake_path/bin:$gnused_path/bin:$perl_path/bin"
 
-    ${if stdenv_pkg.hostPlatform.isDarwin then "export AR=/usr/bin/ar" else ""}
-
-
-    export PATH="$bash_path/bin:$binutils_path/bin:$bison_path/bin:$coreutils_path/bin:$flex_path/bin/:$gawk_path/bin:$gnused_path/bin:$gnugrep_path/bin:$gnumake_path/bin:$gcc_path/bin:$perl_path/bin:$PATH"
-
-    # Dev dependencies
+    # Dev dependencies; necessary only when, say, in interactive nix-shell
+    # environment.
     export PATH="$PATH:$less_path/bin:$which_path/bin"
 
-    # Libraries
-    export LDFLAGS="-L$readline_out_path/lib -L$zlib_out_path/lib" # Don't inherit $LDFLAGS
-    export CFLAGS="-I$readline_dev_path/include -I$zlib_dev_path/include" # Don't inherit $CFLAGS
-    export CPPFLAGS="-I$readline_dev_path/include -I$zlib_dev_path/include" # Don't inherit $CPPFLAGS
+    # Set flags to help find libraries and their header files.
+    #
+    # Note that we _don't_ use the corresponding values, if any, passed to us
+    # in the environment.
+    export LDFLAGS=" -L$readline_out_path/lib     -L$zlib_out_path/lib"
+    export CFLAGS="  -I$readline_dev_path/include -I$zlib_dev_path/include"
+    export CPPFLAGS="-I$readline_dev_path/include -I$zlib_dev_path/include"
 
-    export SHELL # So that pg_regress' Makefile can embed this into the pg_regress binary.
-
-    sh -c 'echo Hello there'
-
-    info cd pg sources
     cd "$PG_MIRROR"
-    info dist-cleaning
-    make distclean || true
-    info configuring
-    ./configure --prefix=$(pwd)/db/ SHELL="$(which sh)"
-    info making
-    make -j 8 install
+
+    ./configure --config-cache --prefix=$(pwd)/db/
     make install
     make check
   '';
 
+  # Check that x is an absolute path.
+  #
+  # Convert the config to path type; this will fail if path does not exist, or
+  # if the path contains illegal characters (per Nix), like a space. Then make
+  # sure that the path starts with a `/`.
+  is_absolute_path = x:
+    isPath (/. + x) && (substring 0 1 x) == "/";
+
 in
 
-  # Ensure that the FARM_ROOT exists, and that it's an absolute path.
-  #
-  # Convert the config to # Path type; this will fail if path does not exist,
-  # or if the path contains illegal (in Nix) characters, like a space. And make
-  # sure that the Farm Root path starts with a `/`;
-  assert isPath (/. + config.FARM_ROOT);
-  assert (substring 0 1 config.FARM_ROOT) == "/";
+  assert is_absolute_path config.FARM_ROOT;
+  assert is_absolute_path config.PG_MIRROR;
 
   derivation {
-    inherit bash_path bison_path coreutils_path flex_path gawk_path gnugrep_path gnumake_path gnused_path gcc_path perl_path;
 
-    inherit readline_out_path readline_dev_path zlib_out_path zlib_dev_path;
+    # Binaries/executables
+    inherit
+      bash_path
+      binutils_path
+      bison_path
+      coreutils_path
+      diffutils_path
+      flex_path
+      gawk_path
+      gnugrep_path
+      gnumake_path
+      gnused_path
+      gcc_path
+      perl_path;
+
+    # Libraries
+    inherit
+      readline_out_path
+      readline_dev_path
+      zlib_out_path
+      zlib_dev_path;
 
     # Dev dependencies
-    inherit less_path which_path;
-
-    #inherit bintools_path;
-    inherit binutils_path;
+    inherit
+      less_path
+      which_path;
 
     FARM_ROOT = config.FARM_ROOT;
     PG_MIRROR = config.PG_MIRROR;
